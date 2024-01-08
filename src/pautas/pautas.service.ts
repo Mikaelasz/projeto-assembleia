ec9289a81d2e1e11159efc2f1288bf4071075a2b
Mikaela Soares <miih.arj@gmail.com>
2024-01-08 21:46:17 +0000

    Refatorando para tornar a lógica mais elegante

--- src/pautas/pautas.service.ts
+++ src/pautas/pautas.service.ts
@@ -1,6 +1,7 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { Pauta } from './pauta.entity';
+import { Result } from 'src/common/result'
 
 @Injectable()
 export class PautasService {
@@ -10,7 +11,7 @@ export class PautasService {
         private readonly pautaRepository: Repository<Pauta>
     ){}
 
-    async save(pauta: Pauta) : Promise<Pauta>{
+    async save(pauta: Pauta) : Promise<Result<Pauta>>{
         const descricao = pauta.descricao;
 
         //busca no banco
@@ -21,11 +22,11 @@ export class PautasService {
         });
 
         if(possivelPauta){
-            throw Error("Pauta existente");
+            return new Result(null, new Error("Pauta existente"));
         }
 
         pauta = await this.pautaRepository.save(pauta)
-        return pauta;
+        return new Result(pauta, null);
     }
 
 }
